c25a30ace5d9893f17fc5588ad981edb7a8fd8b0
Hoscar <oscarodriguez.05.18@gmail.com>
2024-09-14 11:23:30 -0400

    About page

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+# Import the class that you created in views
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('',HomePageView.as_view(), name='home'),
-    
-
+    path('about/', AboutPageView.as_view(), name='about'),
 
 ]
